ef6b387878d8c7e077ad39a6b41a28370843540d
João Gregorini <75985123+jvg21@users.noreply.github.com>
2024-01-29 22:38:22 -0300

    dd

--- maze.py
+++ maze.py
@@ -1,4 +1,6 @@
-class node():
+import sys
+
+class Node():
     def __init__(self,state,parent,action):
         self.state = state
         self.parent = parent
@@ -115,6 +117,78 @@ class Maze():
 
         return result
 
-    
+    def solve(self):
+        self.num_explored = 0
+
+        start = Node(state = self.start,parent=None,action=None)
+        frontier = StackFrontier()
+        frontier.add(start)
+
+        self.explored = set()
+
+        while True:
+
+            if frontier.isEmpty():
+                raise Exception("There is no solution")
+
+            node = frontier.remove()
+            self.num_explored += 1
+
+            if node.state == self.goal:
+                actions = []
+                cells = []
+
+                while node.parent is not None:
+                    cells.append(node.state)
+                    actions.append(node.action)
+                    node = node.parent
+                
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions,cells)
+                return
+            
+            self.explored.add(node.state)
+
+
+            for action,state in self.neighbors(node.state):
+                if not frontier.contains_State(start) and state not in self.explored:
+                    child = Node(state = state, parent = node, action = action)
+                    frontier.add(child)
+
+    def output_image(self,filename,show_solution=True,show_explored=False):
+        from PIL import Image,ImageDraw
+        cell_size = 50
+        call_border = 2
+
+        img = Image.new("RGBA",(self.width*cell_size,self.heigth*cell_size),"black")
+        draw = ImageDraw.Draw(img)
+
+        solution = self.solution[1] if self.solution is not None else None
+        for i, row in enumerate(self.walls):
+            for j, col in enumerate(row):
+                if col:
+                    fill = (40,40,40)
+                elif (i,j) == self.start:
+                    fill = (255,0,0)
+                elif (i,j) == self.goal:
+                    fill = (0,170,28)
+                elif solution is not None and(i,j) in solution:
+                    fill = (220,235,113)
+                else:
+                    fill = (237,240,252)
+
+if len(sys.argv )!=2:
+    sys.exit("Invalid arguments")
+
+maze = Maze(sys.argv[1])
+print("Maze: ")
+maze.print()
+print("Solving")
+maze.solve()
+print("Number of states: ", maze.num_explored)
+print("Solution: ")
+maze.print()
+
 
 
